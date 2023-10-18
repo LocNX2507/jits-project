e553e91a0ae5fe9dc6d1953dfb4135f861f28dab
moonlord2507 <loc20000loc@gmail.com>
2023-10-18 14:35:17 +0700

    feat: config file

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from "vue";
+import Antd from "ant-design-vue";
+import App from "./App";
+import "ant-design-vue/dist/reset.css";
+import "./assets/tailwind.css";
+import router from "./router/index";
+
+const app = createApp(App);
+
+app.use(Antd).use(router).mount("#app");
